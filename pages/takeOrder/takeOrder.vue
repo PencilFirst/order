5455a4fdee5541db3d164ba5f69c253c402d490d
pencilfirst <605204836@qq.com>
2020-07-17 20:40:35 +0800

    完成user页面

---
+++ pages/takeOrder/takeOrder.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		取餐
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/userRank/userRank.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		会员等级
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
